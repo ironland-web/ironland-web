accf1f5a202c52ec8dc6bb6f5a95b729002e29c6
ironland-web <ironland.unoficial@gmail.com>
2024-07-31 16:42:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const currentPath = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        }
+    });
+});
